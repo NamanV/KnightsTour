126cff710416216cdc7342d0bdf4ca8fb8942e98
naman <admin@HARSHITH-MISHRA.local>
2020-05-18 01:47:52 +0530

    executing instructions

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ KnightsTour Problem with custom moves.. Moves are defined in Moves.java file
 
 	- APeiceTour.java class has main method
 	
+# Build and execute Instructions through command line
+
+ - mvn clean
+ - mvn install
+ - mvn exec:java -Dexec.mainClass="dxtr.utility.APeiceTour" -Dexec.args="2 4" 
+   Arguments are the starting position of the peice from (x,y), they have to be integers
--- src/dxtr/utility/APeiceTour.java
+++ src/dxtr/utility/APeiceTour.java
@@ -12,9 +12,10 @@ public class APeiceTour {
 	public static void main(String[] args) {
 		System.out.println("Enter starting position for the piece as two space seperated integers (x,y) : ");
 		Scanner sc = new Scanner(System.in);
-		int startingX = sc.nextInt();
-		int startingY = sc.nextInt();
-		
+//		int startingX = sc.nextInt();
+//		int startingY = sc.nextInt();
+		int startingX = Integer.valueOf(args[0]);
+		int startingY = Integer.valueOf(args[1]);
 		Traversal traversal;
 		try {
 			traversal = new WarnsdorffsHeuristic(new ChessBoard(Constants.BOARD_SIZE, Constants.BOARD_SIZE));
